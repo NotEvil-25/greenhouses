31f789e8965b7b3df413633d4113f856609f2f10
NotEvil-25 <worldwow225@gmail.com>
2020-06-28 18:44:17 +0400

    steps

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,54 @@
-function test(...a) {
-  let footer = document.querySelector('.footer');
-  a.forEach(el => {
-    footer.textContent += el;
-  });
-}
 svg4everybody({
   validate: function (src, svg, use) {
     return true;
   }
 });
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
+function stepsList(target, targetClass, listItem, title, titleMod, listItemContent, listItemContentMod, btn, btnMod){
+  let listItems = document.querySelectorAll(listItem),
+      contents = document.querySelectorAll(listItemContent),
+      buttons = document.querySelectorAll(btn),
+      titles = document.querySelectorAll(title);
+
+  if(target && target.classList.contains(targetClass)){
+    contents.forEach(el =>{ el.classList.remove(listItemContentMod);});
+    buttons.forEach(el =>{ el.classList.remove(btnMod);});
+    titles.forEach(el =>{ el.classList.remove(titleMod);});
+
+    for(let i = 0; i < listItems.length; i++){
+      if(target === listItems[i]){
+        titles[i].classList.add(titleMod);
+        buttons[i].classList.add(btnMod);
+        showEl(contents[i], listItemContentMod);
+      }
+    }
+  }else{
+    for(let i = 0; i < contents.length; i++){
+      hideEl(contents[i], listItemContentMod);
+      titles[i].classList.remove(titleMod);
+      buttons[i].classList.remove(btnMod);
+    }
+  }
+}
+function hideEl(el, classMod){
+  el.classList.remove(classMod);
+}
+function showEl(el, classMod){
+  el.classList.add(classMod);
+}
 document.addEventListener('DOMContentLoaded', () => {
-  let a = 1;
-  let b = 2;
-  console.log(`${a} + ${b} = ${a + b}`);
-  test(1,2,3,4,5);
+  let html = document.querySelector('html');
+
+  html.addEventListener('click', e => {
+    let target = e.target;
+    stepsList(target, 'steps__list-item', '.steps__list-item', '.steps__list-item-content', 'steps__list-item-content--active', '.steps__text', 'steps__text--show', '.steps__btn-more', 'steps__btn-more--active');
+
+  })
 });
--- app/js/script.js
+++ app/js/script.js
@@ -1,25 +1,68 @@
 "use strict";
 
-function test() {
-  var footer = document.querySelector('.footer');
-
-  for (var _len = arguments.length, a = new Array(_len), _key = 0; _key < _len; _key++) {
-    a[_key] = arguments[_key];
-  }
-
-  a.forEach(function (el) {
-    footer.textContent += el;
-  });
-}
-
 svg4everybody({
   validate: function validate(src, svg, use) {
     return true;
   }
 });
+
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
+
+function stepsList(target, targetClass, listItem, title, titleMod, listItemContent, listItemContentMod, btn, btnMod) {
+  var listItems = document.querySelectorAll(listItem),
+      contents = document.querySelectorAll(listItemContent),
+      buttons = document.querySelectorAll(btn),
+      titles = document.querySelectorAll(title);
+
+  if (target && target.classList.contains(targetClass)) {
+    contents.forEach(function (el) {
+      el.classList.remove(listItemContentMod);
+    });
+    buttons.forEach(function (el) {
+      el.classList.remove(btnMod);
+    });
+    titles.forEach(function (el) {
+      el.classList.remove(titleMod);
+    });
+
+    for (var i = 0; i < listItems.length; i++) {
+      if (target === listItems[i]) {
+        titles[i].classList.add(titleMod);
+        buttons[i].classList.add(btnMod);
+        showEl(contents[i], listItemContentMod);
+      }
+    }
+  } else {
+    for (var _i = 0; _i < contents.length; _i++) {
+      hideEl(contents[_i], listItemContentMod);
+
+      titles[_i].classList.remove(titleMod);
+
+      buttons[_i].classList.remove(btnMod);
+    }
+  }
+}
+
+function hideEl(el, classMod) {
+  el.classList.remove(classMod);
+}
+
+function showEl(el, classMod) {
+  el.classList.add(classMod);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-  var a = 1;
-  var b = 2;
-  console.log("".concat(a, " + ").concat(b, " = ").concat(a + b));
-  test(1, 2, 3, 4, 5);
+  var html = document.querySelector('html');
+  html.addEventListener('click', function (e) {
+    var target = e.target;
+    stepsList(target, 'steps__list-item', '.steps__list-item', '.steps__list-item-content', 'steps__list-item-content--active', '.steps__text', 'steps__text--show', '.steps__btn-more', 'steps__btn-more--active');
+  });
 });
